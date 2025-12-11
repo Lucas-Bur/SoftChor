65df1659debfacc1f80b7c98b188d9fb8fdf9432
Lucas Burmeister <lucas.burmeister@gmx.de>
2025-12-11 06:13:44 +0100

    initial commit

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import { neon } from '@neondatabase/serverless'
+import { config } from 'dotenv'
+import { drizzle } from 'drizzle-orm/neon-http'
+
+import * as schema from './schema'
+
+config()
+
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle({ client: sql, schema })
